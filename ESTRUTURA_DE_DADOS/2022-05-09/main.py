ef804c7ac25bd29e619b00244434be50d3f18e61
leandl <leandrocrispim891@gmail.com>
2022-05-11 18:59:52 -0300

    Tarefa de ESTRUTURA DE DADOS - Priority Queue

--- ESTRUTURA_DE_DADOS/2022-05-09/main.py
+++ ESTRUTURA_DE_DADOS/2022-05-09/main.py
@@ -1,4 +1,5 @@
 from queue import Queue
+from priority_queue import PriorityQueue
 
 my_queue = Queue(10)
 
@@ -6,6 +7,9 @@ my_queue.push(10)
 my_queue.push(1)
 my_queue.push(13)
 my_queue.push(5)
+
+print(my_queue)
+
 my_queue.push(19)
 my_queue.push(31)
 my_queue.push(14)
@@ -13,13 +17,38 @@ my_queue.push(11)
 my_queue.push(80)
 my_queue.push(18)
 
+print(my_queue)
 
 my_queue.pop()
 my_queue.pop()
+
+print(my_queue)
 
 my_queue.push(999)
 my_queue.push(9)
 
-
-
 print(my_queue)
+
+
+my_priority_queue = PriorityQueue(10)
+
+my_priority_queue.push(10)
+my_priority_queue.push(14)
+my_priority_queue.push(31)
+my_priority_queue.push(1)
+my_priority_queue.push(13)
+my_priority_queue.push(5)
+my_priority_queue.push(19)
+my_priority_queue.push(11)
+my_priority_queue.push(80)
+my_priority_queue.push(18)
+
+print(my_priority_queue)
+
+print(my_priority_queue.pop())
+print(my_priority_queue.pop())
+print(my_priority_queue.pop())
+print(my_priority_queue.pop())
+print(my_priority_queue.pop())
+
+print(my_priority_queue)
---
+++ ESTRUTURA_DE_DADOS/2022-05-09/priority_queue.py
@@ -0,0 +1,70 @@
+import numpy as np
+
+class PriorityQueueException(Exception):
+  pass
+
+class PriorityQueue:
+
+  def __init__(self, max_elements: int) -> None:
+    if max_elements < 0:
+      raise PriorityQueueException("Minimum value of elements in invalid PriorityQueue")
+
+    self.elements = np.array([None] * max_elements)
+    self.max_elements = max_elements
+    self.index_last_element = -1
+
+  def full(self): 
+    return self.max_elements == self.index_last_element + 1
+
+  def empty(self):
+    return self.index_last_element == -1
+
+  def __get_index_of_first_element_with_highest_priority(self, value: int):
+    if self.index_last_element == -1:
+      return 0
+
+    for index in range(self.index_last_element + 1):
+      if value > self.elements[index]:
+        return index
+
+    return self.index_last_element + 1
+
+  def push(self, new_value: int) -> None:
+    if self.full():
+      raise PriorityQueueException("Maximum elements in PriorityQueue")
+    
+    new_index_last_element = self.index_last_element + 1
+
+    index_frist_more = self.__get_index_of_first_element_with_highest_priority(new_value)
+    if index_frist_more == -1 or index_frist_more == new_index_last_element:
+        self.elements[new_index_last_element] = new_value
+        self.index_last_element = new_index_last_element
+        return
+
+    
+    for index in range(new_index_last_element, index_frist_more, -1):
+        self.elements[index] = self.elements[index - 1]
+
+    self.elements[index_frist_more] = new_value
+    self.index_last_element = new_index_last_element
+
+
+
+  def pop(self):
+    if self.empty():
+      return None
+
+    item = self.elements[0]
+
+    for index in range(0, self.index_last_element):
+      self.elements[index] = self.elements[index + 1]
+
+    self.elements[self.index_last_element] = None
+    self.index_last_element -= 1
+
+    return item
+
+
+  def __str__(self) -> str:
+    str_list = " ".join([ str(x) for x in self.elements if not x == None ])
+    return f"[ {str_list} ]"
