20448787c9fca2ee6962f1b207951dd2c42160a7
leandl <leandrocrispim891@gmail.com>
2026-06-25 10:42:37 -0300

    feat: add heuristc `manhattan_with_linear_conflict`.

--- ESTRUTURA_DE_DADOS/DLC/8-puzzle/heuristic.py
+++ ESTRUTURA_DE_DADOS/DLC/8-puzzle/heuristic.py
@@ -52,3 +52,81 @@ class Heuristic:
                 value += 0 if d == 0 else 1
 
         return value
+
+    def manhattan_with_linear_conflict(self, board_matrix):
+        return self.manhattan(board_matrix) + (
+            2 * self.__count_linear_conflicts(board_matrix)
+        )
+
+
+    def __count_linear_conflicts(self, board_matrix):
+        return (
+            self.__count_row_conflicts(board_matrix)
+            + self.__count_column_conflicts(board_matrix)
+        )
+
+
+    def __count_row_conflicts(self, board_matrix):
+        conflicts = 0
+
+        for row, ROW in enumerate(board_matrix):
+            for left_col, left_tile in enumerate(ROW):
+
+                if left_tile == WHITE_SPACE:
+                    continue
+
+                goal_row_left, goal_col_left = self.__get_index_in_final_board(left_tile)
+
+                if goal_row_left != row:
+                    continue
+
+                for right_col in range(left_col + 1, len(ROW)):
+                    right_tile = ROW[right_col]
+
+                    if right_tile == WHITE_SPACE:
+                        continue
+
+                    goal_row_right, goal_col_right = self.__get_index_in_final_board(right_tile)
+
+                    if goal_row_right != row:
+                        continue
+
+                    if goal_col_left > goal_col_right:
+                        conflicts += 1
+
+        return conflicts
+
+
+    def __count_column_conflicts(self, board_matrix):
+        conflicts = 0
+
+        total_rows = len(board_matrix)
+        total_cols = len(board_matrix[0])
+
+        for col in range(total_cols):
+            for top_row in range(total_rows):
+                top_tile = board_matrix[top_row][col]
+
+                if top_tile == WHITE_SPACE:
+                    continue
+
+                goal_row_top, goal_col_top = self.__get_index_in_final_board(top_tile)
+
+                if goal_col_top != col:
+                    continue
+
+                for bottom_row in range(top_row + 1, total_rows):
+                    bottom_tile = board_matrix[bottom_row][col]
+
+                    if bottom_tile == WHITE_SPACE:
+                        continue
+
+                    goal_row_bottom, goal_col_bottom = self.__get_index_in_final_board(bottom_tile)
+
+                    if goal_col_bottom != col:
+                        continue
+
+                    if goal_row_top > goal_row_bottom:
+                        conflicts += 1
+
+        return conflicts
--- ESTRUTURA_DE_DADOS/DLC/8-puzzle/main.py
+++ ESTRUTURA_DE_DADOS/DLC/8-puzzle/main.py
@@ -2,21 +2,28 @@ from entities.board import Board
 from a_star_algorithm import AStarAlgorithm
 from utils import generate_initial_board, generate_final_board, compare_board
 from heuristic import Heuristic
+from config import WHITE_SPACE
 
 MATRIX_FINAL_BOARD = generate_final_board()
+# MATRIX_FINAL_BOARD = [
+#     ["A", "B", "C"],
+#     ["D", "E", "F"],
+#     ["G", "H", WHITE_SPACE]
+# ]
 
 heuristic = Heuristic(MATRIX_FINAL_BOARD)
 # h = lambda _board: 0
 # h = heuristic.out_of_place
-h = heuristic.manhattan
+# h = heuristic.manhattan
+h = heuristic.manhattan_with_linear_conflict
 
 FINAL_BOARD = Board(MATRIX_FINAL_BOARD, h)
 initial_board = generate_initial_board(FINAL_BOARD, MOVEMENTS=100)
-initial_board = Board([
-    ["_", 3, 5],
-    [1, 2, 7],
-    [6, 4,8]
-], h)
+# initial_board = Board([
+#     ["_", 3, 5],
+#     [1, 2, 7],
+#     [6, 4,8]
+# ], h)
 
 def print_matrix(m):
     print("-=-=-=-")
